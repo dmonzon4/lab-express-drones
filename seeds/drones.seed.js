c2b35a3a44a9c3f495a80aed9615b29cc058524d
Diego Monzon <diego.almg@hotmail.com>
2023-11-06 17:30:59 +0100

    Iteracion 1

--- seeds/drones.seed.js
+++ seeds/drones.seed.js
@@ -1,1 +1,22 @@
 // Iteration #1
+const mongoose = require("mongoose");
+const Drone = require("../models/Drone.model");
+// const allDrone = require("./")
+
+const allDrone = [
+    { name: "Creeper XL 500", propellers: 3, maxSpeed: 12 },
+    { name: "Racer 57", propellers: 4, maxSpeed: 20 },
+    { name: "Courier 3000i", propellers: 6, maxSpeed: 18 }
+  ];
+
+mongoose.connect("mongodb://127.0.0.1:27017/lab-express-drones")
+
+  
+  .then((response) => {
+    console.log(response)
+    return Drone.create(allDrone)
+  })
+  .catch ((err) => {
+    console.log(err)
+  })
+
