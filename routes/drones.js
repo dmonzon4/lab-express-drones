e152da4dfc3a2a8d16d2fe2b3f18c5a6b75d81e8
Alejandro Cuevas <alecuevas1996@gmail.com>
2023-11-06 19:26:06 +0100

    iteration 1 & 2

--- routes/drones.js
+++ routes/drones.js
@@ -5,38 +5,73 @@ const router = express.Router();
 
 // require the Drone model here
 
-router.get('/drones', (req, res, next) => {
+router.get('/drones', async (req, res, next) => {
   // Iteration #2: List the drones
-  Drone.find()
-  .then((response) => {
+  try {
+
+    const response = await Drone.find().select({name: 1})
     console.log(response)
+
     res.render("drones/list.hbs", {
-      allDrone: response
+      allDrones: response
     })
+  }catch(err) {
+    next(err)
+  }
+});
+
+router.get('/drones/create', (req, res, next) => {
+  // Iteration #3: Add a new drone
+  res.render("drones/create-form.hbs", {
+
+  })
+});
+
+router.post('/drones/create', (req, res, next) => {
+  // Iteration #3: Add a new drone
+  console.log(req.body)
+
+  Drone.create({
+    name: req.body.name,
+    propellers: req.body.propellers,
+    maxSpeed: req.body.maxSpeed
+  })
+  .then(() => {
+    res.redirect("/drones")
   })
   .catch((err) => {
     next(err)
   })
 });
 
-router.get('/drones/create', (req, res, next) => {
-  // Iteration #3: Add a new drone
-  // ... your code here
-});
-
-router.post('/drones/create', (req, res, next) => {
-  // Iteration #3: Add a new drone
-  // ... your code here
-});
-
-router.get('/drones/:id/edit', (req, res, next) => {
+router.get('/drones/:id/edit', async (req, res, next) => {
   // Iteration #4: Update the drone
-  // ... your code here
+  
+  try {
+    const droneToEdit = await Drone.findById(req.params.droneId)
+
+    res.render("drones/update-form.hbs", {
+      droneToEdit
+    })
+  } catch(err) {
+    next(err)
+  }
 });
 
-router.post('/drones/:id/edit', (req, res, next) => {
+router.post('/drones/:id/edit', async (req, res, next) => {
   // Iteration #4: Update the drone
-  // ... your code here
+  try {
+    const response = await Drone.findByIdAndUpdate(req.params.droneId, {
+      name: req.body.name,
+      propellers: req.body.propellers,
+      maxSpeed: req.body.maxSpeed
+    })
+
+    res.redirect(`/drone/${req.params.droneId}/edit`)
+
+  } catch(err) {
+    next(err)
+  }
 });
 
 router.post('/drones/:id/delete', (req, res, next) => {
